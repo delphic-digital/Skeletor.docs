b619754f190723e97228a3cada680e0db84f7ebe
Jeff Mills <jeffy9@gmail.com>
2016-06-24 20:22:51 -0400

    theme tweaks

--- deploy.sh
+++ deploy.sh
@@ -78,7 +78,8 @@ parse_args() {
 	default_email=${GIT_DEPLOY_EMAIL:-}
 
 	#repository to deploy to. must be readable and writable.
-	repo=${GIT_DEPLOY_REPO:-origin}
+	# repo=${GIT_DEPLOY_REPO:-origin}
+	repo="https://$GH_TOKEN@github.com/delphic-digital/Skeletor.docs.git"
 
 	#append commit hash to the end of message by default
 	append_hash=${GIT_DEPLOY_APPEND_HASH:-true}
